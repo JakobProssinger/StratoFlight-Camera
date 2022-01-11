65f15a9f11c6a1cb46683e85132729adda6ebf6a
LeonAblinger <leon.ablinger@outlook.at>
2022-01-11 16:31:29 +0100

    Added new servo library & test

---
+++ src/servoEndless.py
@@ -0,0 +1,24 @@
+import time
+
+from board import SCL, SDA
+import busio
+
+from adafruit_motor import servo
+from adafruit_pca9685 import PCA9685
+
+i2c = busio.I2C(SCL, SDA)
+
+pca = PCA9685(i2c)
+pca.frequency = 50
+
+servo = servo.Servo(pca.channels[0], actuation_range=180)
+
+while True:
+    for i in range(180):
+        servo.angle = i
+        time.sleep(0.15)
+    for i in range(180):
+        servo.angle = 180 - i
+        time.sleep(0.15)
+
+pca.deinit()
