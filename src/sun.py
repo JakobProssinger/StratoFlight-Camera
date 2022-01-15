f4f0fff363397faaebdc3fe0cfdbe5d83e9fba4d
LeonAblinger <leon.ablinger@outlook.at>
2022-01-15 20:49:37 +0100

    Added sun position program

---
+++ src/sun.py
@@ -0,0 +1,34 @@
+##############################################################################
+# StratoFlight II - The flying Pi                                            #
+# GY-271 Compass module test with QMC5883L IC                                #
+# File:     compassTest.py                                                   #
+# Version:  0.0.1.0                                                          #
+# Author:   AblL / Leon Ablinger                                             #
+##############################################################################
+
+from sunposition import sunpos
+import time
+from datetime import datetime
+
+##############################################################################
+
+LAT = 47.7919442
+LON = 12.9783463
+ELE = 0
+
+def getSunposition():
+    now = datetime.utcnow()
+    az,zen = sunpos(now, LAT, LON, ELE)[:2]
+    elev = 90 - zen
+    return (az,elev)
+
+
+def main():
+    while True:
+        az,elev = getSunposition()
+        print(az, elev)
+        time.sleep(15)
+
+
+if __name__ == "__main__":
+    main()
