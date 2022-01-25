9c5480f68a475dd189f13a56a6d32d50a2471c39
LeonAblinger <leon.ablinger@outlook.at>
2022-01-25 17:46:09 +0100

    Update imageEngine.py

--- src/imageEngine.py
+++ src/imageEngine.py
@@ -16,6 +16,7 @@ camera = PiCamera()
 camera.rotation = -90
 camera.resolution = camera.MAX_RESOLUTION
 picture_folder = "/home/pi/Desktop/media/engine-images"
+image_counter = 0
 
 ##############################################################################
 
@@ -24,30 +25,33 @@ def get_date_time():
     return date_time
 
 
-def images_lowlight(image_counter):
+def images_lowlight():
+    global image_counter
     iso = 300
-    shutter_speed = 500000
-    brightness = 60
     pre_file_name = picture_folder + "/" + get_date_time() + "-" + \
         "ll-" + str(image_counter) + "-"
 
     while iso <= 800:
         camera.iso = iso
-        camera.shutter_speed = shutter_speed
-        camera.brightness = brightness
+        shutter_speed = 500000
+        while shutter_speed <= 6000000:
+            camera.shutter_speed = shutter_speed
+            brightness = 45
+            while brightness <= 55:
+                camera.brightness = brightness
 
-        camera.capture(pre_file_name + "ISO=" + str(iso) + "ShSp=" + \
-            str(shutter_speed) + "BR=" + str(brightness) + ".jpg")
-        image_counter += 1
+                camera.capture(pre_file_name + "ISO=" + str(iso) + "ShSp=" \
+                    + str(shutter_speed) + "BR=" + str(brightness) + ".jpg")
+                image_counter += 1
+                sleepCounter(5)
 
+                brightness += 1
+            shutter_speed += 500000
         iso += 50
-        shutter_speed += 100000
-        brightness += 10
-
-    return image_counter
 
 
-def images_ISO(image_counter):
+def images_ISO():
+    global image_counter
     iso = 0
     pre_file_name = picture_folder + "/" + get_date_time() + "-" + \
         "ll-" + str(image_counter) + "-"
@@ -60,10 +64,9 @@ def images_ISO(image_counter):
 
         iso += 50
 
-    return image_counter
 
-
-def images_brightness(image_counter):
+def images_brightness():
+    global image_counter
     brightness = 0
     camera.brightness = brightness
     pre_file_name = picture_folder + "/" + get_date_time() + "-" + \
@@ -84,7 +87,8 @@ def sleepCounter(time_s):
     while(time_s != 0):
         os.system('clear')
         print("Waiting " + str(time_s) + " seconds...")
-        print("Free space (mb): " + str(getFreeSpace))
+        print("Free space (mb): " + str(getFreeSpace()))
+        print("Current image counter: " + str(image_counter)
         sleep(1)
         time_s -= 1
 
@@ -98,8 +102,6 @@ def getFreeSpace():
 
 
 def main():
-    image_counter = 0
-
     image_counter = images_brightness(image_counter)
 
     sleepCounter(10)
